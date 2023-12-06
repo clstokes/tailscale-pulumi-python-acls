d11b9f36f36db61507308b0feda2c66b44e2e7e4
Cameron Stokes <cameron@cameronstokes.com>
2023-12-06 13:11:24 -0800

    Add README and comments

---
+++ README.md
@@ -0,0 +1,3 @@
+# pulumi-python-acls
+
+An Infrastructure as Code example managing [Tailscale](https://tailscale.com) network access control lists (ACLs) using [Pulumi](https://www.pulumi.com) and Python.
--- __main__.py
+++ __main__.py
@@ -6,12 +6,20 @@ from io import StringIO
 import pandas as pd
 import pulumi_tailscale as tailscale
 
+#
+# Use your customer data to define ACLs, routes, etc.
+# You can move this to a separate CSV or query this from a database.
+#
 customer_csv = """
 customer1,10.0.100.0/24
 customer2,10.0.101.0/24
 customer3,10.0.102.0/24
 """
 
+#
+# Customize this based on your ACL needs.
+# Note the {% for c in customer_list %} ... {% endfor %} loops to iterate over your customer_csv data from above.
+#
 template_string = """
 {
     "groups": {
@@ -65,12 +73,20 @@ template_string = """
 }
 """
 
+#
+# Parse the customer data
+#
 csvData = pd.read_csv(StringIO(customer_csv), header=None, names=['customerName', 'customerSubnet'])
 data_dict = csvData.to_dict(orient='records')
 
+#
+# Render the template
+#
 template = Template(template_string)
 rendered_json = template.render(customer_list=data_dict)
-
 # print(rendered_json)
 
+#
+# Manage the ACL file via Pulumi
+#
 tailscale.Acl('acl',acl=rendered_json)
